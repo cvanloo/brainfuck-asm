8d2160cf73ef8da428e944db52857f0061dfb053
Colin van Loo <colin@vanloo.ch>
2023-08-29 02:35:51 +0200

    preproccess loops

--- brainfuck.s
+++ brainfuck.s
@@ -31,8 +31,8 @@ _start:
     movq          $0, %r9
     syscall
 
-    movq %rax, %r12 # r12: ptr to input buf
-    xorq %r13, %r13 # r13: amount of bytes read
+    movq %rax, %r12 # r12 = ptr to input buf
+    xorq %r13, %r13 # r13 = amount of bytes read
 
 read_input:
     # read(stdin, buf, count=input len)
@@ -65,12 +65,64 @@ read_input_end:
     movq  %r12, (%rbp)   # input ptr
     movq  %r13, -8(%rbp) # input len
 
-    # write(stdout, & input len, count=8)
-    movq      $1, %rax
-    movq      $1, %rdi
-    lea -8(%rbp), %rsi
-    movq      $8, %rdx
-    syscall
+# *** PRE-PROCESS LOOPS ***
+
+    # stack space needed:   program len for loop mapping
+    #                     + program len / 2 * 8 for temporary loop stack
+    movq %r13, %rax
+    shlq   $2, %rax # / 2 * 8 <=> * 4
+    addq %r13, %rax
+    subq %rax, %rsp
+    # rbp-16     = loop mapping
+    # rbp-16-r13 = loop stack
+
+    movq %r12, %rax # rax = ptr to current input char
+    movq %r13, %rdi # rdi = number of iterations left
+
+    leaq -16(%rbp), %rdx
+    movq      %rdx, %rsi # rsi = ptr to loop mapping
+    subq      %r13, %rdx # rdx = ptr to top of loop stack
+
+preprocess_loops:
+    movq (%rax), %r8
+    movzx  %r8b, %r8
+    cmpb    $'[, %r8b # $'[ = 0x5B
+    je push_loop_start
+    cmpb    $'], %r8b # $'] = 0x5D
+    je create_loop_mapping
+    jmp preprocess_loops_next
+
+push_loop_start:
+    movq %rax, (%rdx)
+    addq   $8, %rdx
+
+    jmp preprocess_loops_next
+
+create_loop_mapping:
+    subq     $8, %rdx
+    movq (%rdx), %r8 #  r8 = address of loop start
+                     # rax = address of loop end
+
+    # map end -> start
+    movq  %rax, %r9
+    subq  %r12, %r9 # r9 = index key for loop mapping
+    addq  %rsi, %r9 # index into loop map
+    movq   %r8, (%r9) # store mapping
+
+    # map start -> end
+    movq   %r8, %r9
+    subq  %r12, %r9
+    addq  %rsi, %r9
+    movq  %rax, (%r9)
+
+    # fall through into preprocess_loops_next
+
+preprocess_loops_next:
+    incq  %rax
+    decq  %rdi
+    testq %rdi, %rdi
+    jnz preprocess_loops
+
 
     # exit(0)
     movq $60, %rax
